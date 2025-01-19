0f89445953db915d793536b26829ddd560e234f6
Yiğit Işkırık <yigitolin@outlook.com>
2025-01-19 14:57:22 +0300

    Add movement improvements

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -1,5 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
@@ -26,8 +30,11 @@ public class playerMovement : MonoBehaviour
 
     [HideInInspector]
     public bool canMove = true;
+    public Transform TR;
 
+    public float friction = 0.1f;
 
+    
     private void Awake()
     {
         instance = this;
@@ -38,6 +45,8 @@ public class playerMovement : MonoBehaviour
         theCam = Camera.main;
 
         activeMoveSpeed = movementSpeed;
+
+        TR = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -54,34 +63,38 @@ public class playerMovement : MonoBehaviour
 
             //transform.position += new Vector3(moveInput.x * Time.deltaTime * movementSpeed, moveInput.y * Time.deltaTime * movementSpeed, 0f);
 
-            theRB.linearVelocity = moveInput * activeMoveSpeed;
+            //theRB.linearVelocity = moveInput * activeMoveSpeed;
+            float theX = Mathf.Lerp(theRB.linearVelocity.x, moveInput.x * activeMoveSpeed, friction * Time.deltaTime);
+            float theY = Mathf.Lerp(theRB.linearVelocity.y, moveInput.y * activeMoveSpeed, friction * Time.deltaTime);
+            theRB.linearVelocity = new Vector2(theX, theY);
 
-            Vector3 mousePos = Input.mousePosition;
-            Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition);
-
-            /*if (mousePos.x < screenPoint.x)
+            if (moveInput.x > 0)
             {
-                transform.localScale = new Vector3(-1f, 1f, 1f);
-                gunArm.localScale = new Vector3(-1f, -1f, 1f);
+                SR.sprite = sprites[1];
+                TR.eulerAngles = new Vector3(0f, 0f, 0f);
             }
-            else
+            else if (moveInput.x < 0)
             {
-                transform.localScale = Vector3.one;
-                gunArm.localScale = Vector3.one;
+                
+                 SR.sprite = sprites[1];
+                 TR.eulerAngles = new Vector3(0f, 180f, 0f);
             }
-            */
-
-            if (Input.GetKeyDown(KeyCode.Space))
+            else if (moveInput.y < 0)
             {
-                if (dashCoolCounter <= 0 && dashCounter <= 0)
-                {
+                SR.sprite = sprites[0];
+            }
+            else if (moveInput.y > 0)
+            {
+                SR.sprite = sprites[2];
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.Space) && dashCoolCounter <= 0 && dashCounter <= 0)
+            {
                     activeMoveSpeed = dashSpeed;
                     dashCounter = dashLenght;
 
                     //Anim.SetTrigger("Dash");
-                }
-
-
             }
             if (dashCounter > 0)
             {
@@ -107,10 +120,16 @@ public class playerMovement : MonoBehaviour
                 //Anim.SetBool("IsMoving", false);
             }
         }
-        else
+        /*else
         {
-            theRB.linearVelocity = Vector2.zero;
+            if (theRB.linearVelocity.magnitude == 0) return;
+            float x = theRB.linearVelocity.x - Time.deltaTime * friction;
+            float y = theRB.linearVelocity.y - Time.deltaTime * friction;
+            if (x < 0) x = 0;
+            if (y < 0) y = 0;
+            theRB.linearVelocity = new Vector2(x, y);
+            
             //Anim.SetBool("IsMoving", false);
-        }   
+        }*/
     }
 }
